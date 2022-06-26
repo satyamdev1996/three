d4ae167578994bd500341e450d6348cbed9150eb
Satyam <satyamdevb@chetu.com>
2022-06-26 23:57:35 +0530

    sun and mercury

--- three/main.js
+++ three/main.js
@@ -1,7 +1,10 @@
-import * as Three from "https://unpkg.com/three@0.126.1/build/three.module.js"
-
-
+import * as Three from 'three';
+import suntex from './images/sun.jpg'
+import mertex from './images/mercury.jpg'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+// console.log(OrbitControls)
 const scene = new Three.Scene()
+const textureLoader = new Three.TextureLoader();
 console.log(scene)
 const camera = new Three.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
 console.log(camera)
@@ -12,12 +15,34 @@ renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new Three.PlaneGeometry(5, 5, 10, 10);
-const material = new Three.MeshPhongMaterial({ color: 0x00ff00, side: Three.DoubleSide });
+
+
+const particleGeometry = new Three.BufferGeometry;
+const particleCount = 3000;
+const posArray = new Float32Array(particleCount * 3);
+for (let i = 0; i < particleCount * 3; i++) {
+  posArray[i] = (Math.random() - 0.5) * 2000
+}
+particleGeometry.setAttribute('position', new Three.BufferAttribute(posArray, 3))
+const mat = new Three.PointsMaterial({
+  size: 0.005
+})
+
+const particlemesh = new Three.Points(particleGeometry, mat)
+const geometry = new Three.SphereGeometry(16, 30, 30);
+const material = new Three.MeshPhongMaterial({ map: textureLoader.load(suntex), side: Three.DoubleSide });
 const sphere = new Three.Mesh(geometry, material);
+
+
+const merGeometry = new Three.SphereGeometry(3.2, 30, 30);
+const MerMaterial = new Three.MeshStandardMaterial({ map: textureLoader.load(mertex), side: Three.DoubleSide });
+const MerSphere = new Three.Mesh(merGeometry, MerMaterial);
+MerSphere.position.x = 28
+sphere.add(MerSphere)
 scene.add(sphere)
+scene.add(particlemesh)
 console.log(geometry)
-camera.position.z = 35
+camera.position.z = 100
 renderer.render(scene, camera)
 
 
@@ -25,12 +50,31 @@ const light = new Three.DirectionalLight(0xffffff, 1)
 light.position.set(0, 0, 10)
 scene.add(light)
 
+document.addEventListener('mousemove', animateParticles)
+let mouseX = 0;
+let mouseY = 0;
+function animateParticles(event) {
+  mouseX = event.clientY
+  mouseY = event.clientX
+}
+const clock = new Three.Clock()
 
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  particlemesh.rotation.y = -mouseX * (elapsedTime * 0.00005)
+  particlemesh.rotation.x = -mouseY * (elapsedTime * 0.00005)
+
+
+}
 
 function animate() {
   requestAnimationFrame(animate)
+  tick()
   renderer.render(scene, camera)
-  sphere.rotation.x += 0.01
+  // sphere.rotation.x += 0.001
+  sphere.rotation.y += 0.007
+  MerSphere.rotation.y += 0.05
+
   // sphere.rotation.y += 0.01
 
 }
